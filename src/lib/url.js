d5585fb85d86fe8d9a6457129f0904e7414b9173
wyj <1143246342@qq.com>
2020-05-23 14:15:25 +0800

    推荐功能

--- src/lib/url.js
+++ src/lib/url.js
@@ -1,3 +1,5 @@
 // export const DEVELOP_URL = 'http://localhost:3721/api/'
 
-export const DEVELOP_URL = 'http://106.52.212.170:8888/'
+// export const DEVELOP_URL = 'http://106.52.212.170:8888/'
+
+export const DEVELOP_URL = 'https://jcircle.jarwu.top/';
